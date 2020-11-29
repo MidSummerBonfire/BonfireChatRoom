70804b74d8f3cd44efd613d42304a7d901b3a7ff
MidSummerBonfire <74707470+MidSummerBonfire@users.noreply.github.com>
2020-11-28 18:20:16 -0800

    init commit
    added
    index.php -needs jq 1.3min
    css folder
    script folder

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE>
+<html>
+    <head>
+        <title>Bonfire Chat</title>
+        <link type="text/css" rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+    <div id="wrapper">
+        
+        <div id="menu">
+            <p class="welcome">hello dark world<b></b></p>
+            <p class="logout"><a id="exit" href="#">exit chat</a></p>
+            <div style="clear:both"></div>
+        </div>
+        
+        <div id="chatbox"></div>
+        
+        <form name="message" action="">
+            <input name="usermsg" type="text" id="usermsg" size="63">
+            <input name="submitmsg" type="submit" id="submitmsg" value="Send">
+        </form>
+            
+    </div>
+    
+    <script type="text/javascript" src="place holder jquery/1.3"></script>
+    <script type="text/javascript">
+    $(document).ready(function() {
+    
+    });
+    </script>
+    </body>
+</html>
